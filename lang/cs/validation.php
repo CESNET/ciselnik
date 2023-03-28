7acfcec41181272631a7b604b4a05accba695082
Jan Oppolzer <jan@oppolzer.cz>
2023-03-28 15:54:12 +0200

    Fix lang/cs/validation.php

--- lang/cs/validation.php
+++ lang/cs/validation.php
@@ -138,6 +138,7 @@ return [
         'ou;lang-en' => 'organizační jednotka v angličtině',
         'ouabbrev;lang-cs' => 'zkratka organizační jednotky v češtině',
         'ouabbrev;lang-en' => 'zkratka organizační jednotky v angličtině',
+        'ico' => 'identifikační číslo osoby (IČO)',
         'postalcode' => 'PSČ',
         'street' => 'ulice',
     ],
