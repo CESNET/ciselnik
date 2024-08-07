57f33e3ec84b40b5e7badd1a5d3dee6d9b80eae1
Jan Oppolzer <jan@oppolzer.cz>
2024-08-07 11:00:09 +0200

    Fix health check

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('language/{language}', LanguageController::class);
 
+Route::view('up', 'health-up');
 Route::view('/', 'welcome')->middleware('guest');
 
 Route::middleware('auth')->group(function () {
